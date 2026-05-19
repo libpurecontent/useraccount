68b6c580d5eae7fd6f01f312ca546443d8c2bf7b
mvl22 <martin@lucas-smith.co.uk>
2026-05-19 19:51:24 +0100

    Migrate setcookie to PHP 7.3+ syntax

--- src/userAccount.php
+++ src/userAccount.php
@@ -510,7 +510,14 @@ class userAccount
 		
 		if (ini_get ('session.use_cookies')) {
 			$params = session_get_cookie_params ();
-			setcookie (session_name (), '', time () - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+			$options = array (
+				'expires' => time () - 42000,
+				'path' => $params['path'],
+				'domain' => $params['domain'],
+				'secure' => $params['secure'],
+				'httponly' => $params['httponly'],
+			);
+			setcookie (session_name (), '', $options);
 		}
 	}
 	
